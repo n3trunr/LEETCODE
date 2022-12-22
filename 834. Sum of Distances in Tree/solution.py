47021c3c5bb26c4e69bea309724a06fe62566ab3
ez3mf <118151594+n3trunr@users.noreply.github.com>
2022-12-22 09:57:07 -0500

    Tasks

--- 834. Sum of Distances in Tree/solution.py
+++ 834. Sum of Distances in Tree/solution.py
@@ -1,3 +1,11 @@
+'''
+Tasks: 
+1. Create Test Cases
+2. Create Build
+3. Run Build
+4. Finish soluion.py
+'''
+
 class Solution:
     def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
         
